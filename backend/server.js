b46aa13f1b13e3b039f319d9dbc2d342a3578aaf
truptibc23169-arch <truptib_c23169@students.isquareit.edu.in>
2026-04-14 23:45:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(",").map(s => s.trim()) : true,
+    origin: "*",
   })
 );
 
